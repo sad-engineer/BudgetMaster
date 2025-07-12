bccf54ecde041ce0abc943419a0685504247dc43
Andrey Nikolaevich Korenyuk <korenyuk.a.n@mail.ru>
2025-07-12 15:51:21 +0500

    Version 0.0.015
    Реструктуризация проекта: перенес Android приложение в android-app/, добавил backend"d/,

--- budget_master/repositories/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# ---------------------------------------------------------------------------------------------------------------------
-from budget_master.repositories.sqlite_repository import SQLiteRepository
-
-__all__ = [
-    'SQLiteRepository',
-]
--- budget_master/temp/2.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-timestamp_ms = 1751624270538
-dt = datetime.fromtimestamp(timestamp_ms / 1000)
-print(dt)  # 2025-07-04 15:17:50.538000
